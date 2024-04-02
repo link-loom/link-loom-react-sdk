9c605a91d152f08ad011cb6b87907b4a727eb6c9
Camilo Rodriguez Cuaran <camiepisode@outlook.com>
2024-04-02 10:11:10 -0500

    Fix: Updated config

--- rollup.config.cjs
+++ rollup.config.cjs
@@ -1,13 +1,12 @@
 const resolve = require('@rollup/plugin-node-resolve');
 const commonjs = require('@rollup/plugin-commonjs');
-const babel = require('@rollup/plugin-babel');
+const babel = require('@rollup/plugin-babel').default;
 const alias = require('@rollup/plugin-alias');
-const builtins = require('rollup-plugin-node-builtins');
 const path = require('path');
 const postcss = require('rollup-plugin-postcss');
 const json = require('@rollup/plugin-json');
-const pkg = require('./package.json');
 const peerDepsExternal = require('rollup-plugin-peer-deps-external');
+const replace = require('@rollup/plugin-replace');
 
 module.exports = {
   input: 'src/index.js',
@@ -15,37 +14,49 @@ module.exports = {
     {
       file: 'dist/ui-sdk.cjs.js',
       format: 'cjs',
+      sourcemap: true, 
     },
     {
       file: 'dist/ui-sdk.esm.js',
       format: 'esm',
+      sourcemap: true, 
     },
   ],
   onwarn: function(warning, warn) {
-    if (warning.message && warning.message.includes('use client')) {
+    if (warning.message && warning.message.includes('use client')){
       return;
     }
-
+    if (warning.code === 'CIRCULAR_DEPENDENCY' || warning.code === 'UNUSED_EXTERNAL_IMPORT') {
+      return;
+    }
     warn(warning);
   },
   plugins: [
-    peerDepsExternal(),
+    peerDepsExternal(), 
     alias({
       entries: [
         { find: '@components', replacement: path.resolve(__dirname, 'src/components') },
       ],
     }),
     resolve({
-      browser: true,
-      preferBuiltins: false,
-    }),    
-    commonjs(),
-    babel({ babelHelpers: 'bundled' }),
-    postcss({
-      extract: true,
+      browser: true, 
+      preferBuiltins: false, 
+      extensions: ['.mjs', '.js', '.jsx', '.json', '.node'], 
     }),
-    json(),
-    builtins()
+    commonjs(), 
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**', 
+      extensions: ['.js', '.jsx'], 
+    }),
+    postcss({
+      extract: true, 
+    }),
+    json(), 
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+      preventAssignment: true,
+    })
   ],
-  external: ["react", "react-dom"],
+  external: ['react', 'react-dom'], 
 };
