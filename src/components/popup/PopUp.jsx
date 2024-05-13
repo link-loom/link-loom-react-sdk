e7102f4c18aabf1a163d79962e8af15882643bb7
Camilo Rodriguez Cuaran <camiepisode@outlook.com>
2024-05-12 22:52:21 -0500

    Feat: Improved popup look and feel

--- src/components/popup/PopUp.jsx
+++ src/components/popup/PopUp.jsx
@@ -1,8 +1,16 @@
 import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
 import React from 'react';
+import styled from 'styled-components';
+
+const CloseButton = styled(IconButton)`
+  z-index: 99;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  height: 40px;
+`;
 
 const PopUp = ({ children, title, id, isOpen, setIsOpen, styles }) => {
   const handleClose = () => setIsOpen(false);
@@ -18,17 +26,17 @@ const PopUp = ({ children, title, id, isOpen, setIsOpen, styles }) => {
       disableRestoreFocus
     >
       <Box
-        className="position-absolute top-50 start-50 translate-middle bg-white p-3"
+        className="position-absolute top-50 start-50 translate-middle bg-white"
         style={styles}
       >
-        <IconButton className="position-absolute top-0 end-0" onClick={handleClose}>
+        <CloseButton onClick={handleClose}>
           <i className="mdi mdi-close"></i>
-        </IconButton>
+        </CloseButton>
 
         <Typography id="modal-modal-title" variant="h6" component="h2">
           {title}
         </Typography>
-        <Box className="my-3 overflow-auto" style={{ maxHeight: '75vh' }}>
+        <Box className="overflow-auto" style={{ maxHeight: '80vh' }}>
           {children}
         </Box>
       </Box>
