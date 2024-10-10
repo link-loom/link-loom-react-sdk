e39464dcabb8029f0bfbc18d5ebb32b181a2fd10
Camilo Rodriguez Cuaran <camiepisode@outlook.com>
2024-10-10 18:40:59 -0500

    Feat: Removed react router

--- src/hooks/useNavigate.js
+++ src/hooks/useNavigate.js
@@ -1,15 +1,12 @@
-import { useNavigate as reactRouterUseNavigate } from 'react-router-dom';
 
 const useNavigate = () => {
-  const navigate = reactRouterUseNavigate();
-
   const customNavigate = (url, options = {}) => {
     const { newTab = false } = options;
 
     if (newTab) {
       window.open(url, '_blank');
     } else {
-      navigate(url);
+      window.location.href = url;
     }
   };
 
